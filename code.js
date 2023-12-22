5185d76a71e3b093034f6653932629bf98f68786
Sanjay Sharma <sharma.sanjay813@gmail.com>
2023-12-22 08:36:06 +0530

    [optimized one]1637. Widest Vertical Area Between Two Points Containing No Points

--- code.js
+++ code.js
@@ -21,7 +21,7 @@ points[i].length == 2
 0 <= xi, yi <= 109
 */
 
-// Solution 
+// Solution 1
 
 /**
  * @param {number[][]} points
@@ -53,4 +53,23 @@ points[i].length == 2
 
     return maximumDiff;
 
+};
+
+
+// Solution 2
+
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxWidthOfVerticalArea = function(points) {
+    points = points.sort((a, b) => b[0] - a[0]);
+    let maximumDiff = 0;
+    for (let i = 0; i < points.length -1; i++) {
+        let diff = points[i][0] - points[i+1][0];
+        if (diff > maximumDiff) {
+            maximumDiff = diff;
+        }
+    }
+    return maximumDiff; 
 };
