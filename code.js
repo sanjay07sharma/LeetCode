55139f8cbb6956d28239f9c92861a21cfc3ad5b4
Sanjay Sharma <sharma.sanjay813@gmail.com>
2023-12-28 23:09:57 +0530

    13. Roman to Integer

--- code.js
+++ code.js
@@ -1,57 +1,134 @@
 /*
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
-Given an integer x, return true if x is a 
-palindrome
-, and false otherwise.
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
 
  
 
 Example 1:
 
-Input: x = 121
-Output: true
-Explanation: 121 reads as 121 from left to right and from right to left.
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
 Example 2:
 
-Input: x = -121
-Output: false
-Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
 Example 3:
 
-Input: x = 10
-Output: false
-Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
  
 
 Constraints:
 
--231 <= x <= 231 - 1
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 */
 
 /**
- * @param {number} x
- * @return {boolean}
- */
+ * @param {string} s
+ * @return {number}
+*/
 
 //solution 1
 
-// number to array to string then compare.
+var romanToInt = function(s) {
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
-var isPalindrome = function(x) {
-    let arr = Array.from((x).toString());
-    return (arr.toString() === (arr.reverse()).toString()) ? true : false
+    let sum = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const current = romanValues[s[i]];
+        const next = romanValues[s[i + 1]];
+
+        if (next > current) {
+            sum += next - current;
+            i++; // Skip the next character, as it has been considered already
+        } else {
+            sum += current;
+        }
+    }
+
+    return sum;
 };
+
 
 
 //solution 2
 
 /**
- * @param {number} x
- * @return {boolean}
- */
+ * @param {string} s
+ * @return {number}
+*/
 
-// number to string to array to reverse to string to number.
-
-var isPalindrome = function(x) {
-    return parseInt((x.toString()).split('').reverse().join('')) === x ? true : false;
+var romanToInt = function(s) {
+    let sum = 0;
+    for (let i=0; i<s.length; i++) {
+        switch (s[i]) {
+            case 'I' : {
+                sum += 1;
+                break;
+            }
+            case 'V' : {
+                sum += 5;
+                break;
+            }
+            case 'X' : {
+                sum += 10;
+                break;
+            }
+            case 'L' : {
+                sum += 50;
+                break;
+            }
+            case 'C' : {
+                sum += 100;
+                break;
+            }
+            case 'D' : {
+                sum += 500;
+                break;
+            }
+            case 'M' : {
+                sum += 1000;
+                break;
+            }
+        }
+        
+        if (s[i] === 'I' && ['V', 'X'].includes(s[i+1])) {
+            sum -=2;
+        } else if (s[i] === 'X' && ['L', 'C'].includes(s[i+1])) {
+            sum -=20;
+        } else if (s[i] === 'C' && ['D', 'M'].includes(s[i+1])) {
+            sum -=200
+        }
+    }
+    return sum;
 };
