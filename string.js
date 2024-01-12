f81aabdb44f60a4042a684f7f6e8ad807aabe1b3
Sanjay Sharma <sharma.sanjay813@gmail.com>
2024-01-12 08:25:03 +0530

    1704. Determine if String Halves Are Alike

--- string.js
+++ string.js
@@ -194,3 +194,46 @@ var longestPalindrome = function(s) {
     // Return the longest palindrome substring
     return s.substring(start, end);
 };
+
+/*
+You are given a string s of even length. Split this string into two halves of equal lengths,
+and let a be the first half and b be the second half.
+
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U').
+Notice that s contains uppercase and lowercase letters.
+
+Return true if a and b are alike. Otherwise, return false.
+
+
+Example 1:
+
+Input: s = "book"
+Output: true
+Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+Example 2:
+
+Input: s = "textbook"
+Output: false
+Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+Notice that the vowel o is counted twice.
+
+Constraints:
+
+2 <= s.length <= 1000
+s.length is even.
+s consists of uppercase and lowercase letters.
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function getVovelFromString(s) {
+    return s.split('').filter(char => 'aeiouAEIOU'.includes(char)).join('');
+}
+var halvesAreAlike = function(s) {
+    let firstHalf = getVovelFromString(s.slice(0, Math.ceil(s.length / 2)));
+    let secondHalf = getVovelFromString(s.slice(Math.ceil(s.length / 2)));
+
+    return (firstHalf.length === secondHalf.length) ? true : false;
+};
